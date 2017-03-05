8d66e11f26ec36d3cd5dfb37f85388228cb8b031
Nayaata <n.m.keranova@gmail.com>
2017-03-05 23:14:57 +0200

    Tab navigation

--- Beergram/app/src/main/java/com/example/dreamteam/beergram/tabs/TabActivity.java
+++ Beergram/app/src/main/java/com/example/dreamteam/beergram/tabs/TabActivity.java
@@ -1,128 +1,195 @@
-package com.example.dreamteam.beergram.tabs;
-
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.support.v7.widget.Toolbar;
-        import android.view.View;
-        import android.widget.Button;
-        import com.example.dreamteam.beergram.R;
-
-public class TabActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private Toolbar toolbar;
-    private Button btnSimpleTabs, btnIconTabs;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tab);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        btnSimpleTabs = (Button) findViewById(R.id.btnSimpleTabs);
-        btnIconTabs = (Button) findViewById(R.id.btnIconTabs);;
-
-        btnSimpleTabs.setOnClickListener(this);
-        btnIconTabs.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.btnSimpleTabs:
-                startActivity(new Intent(TabActivity.this, SimpleTabsActivity.class));
-                break;
-            case R.id.btnIconTabs:
-                startActivity(new Intent(TabActivity.this, IconTabsActivity.class));
-                break;
-        }
-    }
-}
-
-/*
-import android.app.ActionBar;
-
-
-import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
-
-import com.example.dreamteam.beergram.R;
-
-public class TabActivity extends AppCompatActivity implements ActionBar.TabListener {
-    public static TabActivity instance;
-    private OneFragment fragmentOne;
-    private TwoFragment fragmentTwo;
-    private TabLayout allTabs;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        instance=this;
-        setContentView(R.layout.activity_tab);
-        getAllWidgets();
-        setupTabLayout();
-    }
-    public static TabActivity getInstance() {
-        return instance;
-    }
-    private void getAllWidgets() {
-        allTabs = (TabLayout) findViewById(R.id.tabs);
-    }
-    private void setupTabLayout() {
-        fragmentOne = new OneFragment();
-        fragmentTwo = new TwoFragment();
-        allTabs.addTab(allTabs.newTab().setText("ONE"),true);
-        allTabs.addTab(allTabs.newTab().setText("TWO"));
-    }
-
-    private void setCurrentTabFragment(int tabPosition)
-    {
-        switch (tabPosition)
-        {
-            case 0 :
-                replaceFragment(fragmentOne);
-                break;
-            case 1 :
-                replaceFragment(fragmentTwo);
-                break;
-        }
-    }
-    public void replaceFragment(Fragment fragment) {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.replace(R.id.frame_container, fragment);
-        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-        ft.commit();
-    }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
-        setCurrentTabFragment(tab.getPosition());
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
-
-    }
-}
-*/
+//package com.example.dreamteam.beergram.tabs;
+//
+//        import android.content.Intent;
+//        import android.os.Bundle;
+//        import android.support.design.widget.TabLayout;
+//        import android.support.v4.app.Fragment;
+//        import android.support.v4.app.FragmentManager;
+//        import android.support.v4.app.FragmentPagerAdapter;
+//        import android.support.v4.view.ViewPager;
+//        import android.support.v7.app.AppCompatActivity;
+//        import android.support.v7.widget.Toolbar;
+//        import android.view.View;
+//        import android.widget.Button;
+//        import com.example.dreamteam.beergram.R;
+//
+//        import java.util.ArrayList;
+//        import java.util.List;
+////import info.androidhive.materialtabs.R;
+//
+//public class TabActivity extends AppCompatActivity { //extends AppCompatActivity implements View.OnClickListener {
+//    private Toolbar toolbar;
+//    private TabLayout tabLayout;
+//    private ViewPager viewPager;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_tab);
+//
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//        viewPager = (ViewPager) findViewById(R.id.viewpager);
+//        setupViewPager(viewPager);
+//
+//        tabLayout = (TabLayout) findViewById(R.id.tabs);
+//        tabLayout.setupWithViewPager(viewPager);
+//    }
+//
+//    private void setupViewPager(ViewPager viewPager) {
+//        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+//        adapter.addFragment(new OneFragment(), "ONE");
+//        adapter.addFragment(new TwoFragment(), "TWO");
+//        adapter.addFragment(new ThreeFragment(), "THREE");
+//        viewPager.setAdapter(adapter);
+//    }
+//
+//    class ViewPagerAdapter extends FragmentPagerAdapter {
+//        private final List<Fragment> mFragmentList = new ArrayList<>();
+//        private final List<String> mFragmentTitleList = new ArrayList<>();
+//
+//        public ViewPagerAdapter(FragmentManager manager) {
+//            super(manager);
+//        }
+//
+//        @Override
+//        public Fragment getItem(int position) {
+//            return mFragmentList.get(position);
+//        }
+//
+//        @Override
+//        public int getCount() {
+//            return mFragmentList.size();
+//        }
+//
+//        public void addFragment(Fragment fragment, String title) {
+//            mFragmentList.add(fragment);
+//            mFragmentTitleList.add(title);
+//        }
+//
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            return mFragmentTitleList.get(position);
+//        }
+//    }
+//}
+///*
+//    private Toolbar toolbar;
+//    private Button btnSimpleTabs, btnIconTabs;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_tab);
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        btnSimpleTabs = (Button) findViewById(R.id.btnSimpleTabs);
+//        btnIconTabs = (Button) findViewById(R.id.btnIconTabs);;
+//
+//        btnSimpleTabs.setOnClickListener(this);
+//        btnIconTabs.setOnClickListener(this);
+//
+//    }
+//
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.btnSimpleTabs:
+//                startActivity(new Intent(TabActivity.this, SimpleTabsActivity.class));
+//                break;
+//            case R.id.btnIconTabs:
+//                startActivity(new Intent(TabActivity.this, IconTabsActivity.class));
+//                break;
+//        }
+//    }
+//}
+//*/
+///*
+//import android.app.ActionBar;
+//
+//
+//import android.os.Bundle;
+//import android.support.design.widget.TabLayout;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.FragmentPagerAdapter;
+//import android.support.v4.app.FragmentTransaction;
+//import android.support.v4.view.ViewPager;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.LayoutInflater;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import android.widget.TextView;
+//
+//import com.example.dreamteam.beergram.R;
+//
+//public class TabActivity extends AppCompatActivity implements ActionBar.TabListener {
+//    public static TabActivity instance;
+//    private OneFragment fragmentOne;
+//    private TwoFragment fragmentTwo;
+//    private TabLayout allTabs;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        instance=this;
+//        setContentView(R.layout.activity_tab);
+//        getAllWidgets();
+//        setupTabLayout();
+//    }
+//    public static TabActivity getInstance() {
+//        return instance;
+//    }
+//    private void getAllWidgets() {
+//        allTabs = (TabLayout) findViewById(R.id.tabs);
+//    }
+//    private void setupTabLayout() {
+//        fragmentOne = new OneFragment();
+//        fragmentTwo = new TwoFragment();
+//        allTabs.addTab(allTabs.newTab().setText("ONE"),true);
+//        allTabs.addTab(allTabs.newTab().setText("TWO"));
+//    }
+//
+//    private void setCurrentTabFragment(int tabPosition)
+//    {
+//        switch (tabPosition)
+//        {
+//            case 0 :
+//                replaceFragment(fragmentOne);
+//                break;
+//            case 1 :
+//                replaceFragment(fragmentTwo);
+//                break;
+//        }
+//    }
+//    public void replaceFragment(Fragment fragment) {
+//        FragmentManager fm = getSupportFragmentManager();
+//        FragmentTransaction ft = fm.beginTransaction();
+//        ft.replace(R.id.frame_container, fragment);
+//        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+//        ft.commit();
+//    }
+//
+//    @Override
+//    public void onTabSelected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+//        setCurrentTabFragment(tab.getPosition());
+//    }
+//
+//    @Override
+//    public void onTabUnselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+//
+//    }
+//
+//    @Override
+//    public void onTabReselected(ActionBar.Tab tab, android.app.FragmentTransaction ft) {
+//
+//    }
+//}
+//*/
--- Beergram/app/src/main/java/com/example/dreamteam/beergram/tabs/ThreeFragment.java
+++ Beergram/app/src/main/java/com/example/dreamteam/beergram/tabs/ThreeFragment.java
@@ -1,48 +1,49 @@
-package com.example.dreamteam.beergram.tabs;
-
-
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import com.example.dreamteam.beergram.R;
-
-public class ThreeFragment extends Fragment{
-
-    public ThreeFragment() {
-        // Required empty public constructor
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three, container, false);
-    }
-
-}
-
-
 //package com.example.dreamteam.beergram.tabs;
+//
 //
 //import android.os.Bundle;
 //import android.support.v4.app.Fragment;
 //import android.view.LayoutInflater;
 //import android.view.View;
 //import android.view.ViewGroup;
-//
 //import com.example.dreamteam.beergram.R;
+////import info.androidhive.materialtabs.R;
 //
-//public class BlankFragment3 extends Fragment {
+//public class ThreeFragment extends Fragment{
+//
+//    public ThreeFragment() {
+//        // Required empty public constructor
+//    }
 //
 //    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_three, container, false);
 //    }
+//
 //}
+//
+//
+////package com.example.dreamteam.beergram.tabs;
+////
+////import android.os.Bundle;
+////import android.support.v4.app.Fragment;
+////import android.view.LayoutInflater;
+////import android.view.View;
+////import android.view.ViewGroup;
+////
+////import com.example.dreamteam.beergram.R;
+////
+////public class BlankFragment3 extends Fragment {
+////
+////    @Override
+////    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+////        return inflater.inflate(R.layout.fragment_three, container, false);
+////    }
+////}
