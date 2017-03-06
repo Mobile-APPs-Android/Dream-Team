cce8fbae9e1d1a7c51d3be22e038af00b39a1d82
Georgi Karaboichev <djorkaevhell@abv.bg>
2017-03-06 13:15:56 +0200

    Refactor User

--- Beergram/app/src/main/java/com/example/dreamteam/beergram/models/User.java
+++ Beergram/app/src/main/java/com/example/dreamteam/beergram/models/User.java
@@ -15,6 +15,9 @@ public class User {
 
     private String address;
 
+    public User() {
+    }
+
     public User(String email, String firstName, String lastName, String address) {
         this.setEmail(email);
         this.setFirstName(firstName);
